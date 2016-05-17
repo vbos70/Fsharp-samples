c550e791f0587aa1551878fbefb9145c054a7819
Victor <victor.bos@gmail.com>
2016-05-17 13:43:24 +0300

    quicksort from fsharpforfunandprofit added.

--- sample1/sample1/Program.fs
+++ sample1/sample1/Program.fs
@@ -1,6 +1,23 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+let rec quicksort list =
+   match list with
+   | [] ->                            // If the list is empty
+        []                            // return an empty list
+   | firstElem::otherElements ->      // If the list is not empty     
+        let smallerElements =         // extract the smaller ones    
+            otherElements             
+            |> List.filter (fun e -> e < firstElem) 
+            |> quicksort              // and sort them
+        let largerElements =          // extract the large ones
+            otherElements 
+            |> List.filter (fun e -> e >= firstElem)
+            |> quicksort              // and sort them
+        // Combine the 3 parts into a new list and return it
+        List.concat [smallerElements; [firstElem]; largerElements]
+
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
